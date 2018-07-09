7179e7d1ae50fd205db8581c718fce1afe2def85
Tiu <Tiu@DESKTOP-IFN6AMP>
2018-07-09 16:24:59 +0900

    変更

--- g_test/src/g_test/test1.java
+++ g_test/src/g_test/test1.java
@@ -4,10 +4,6 @@ public class test1 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("time is 03:37");
-<<<<<<< HEAD
 		System.out.println("is master");
-=======
-		System.out.println("is time");
->>>>>>> refs/heads/time
 	}
 }
